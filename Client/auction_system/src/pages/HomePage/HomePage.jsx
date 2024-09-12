b3c6fb59eff2c33b1a37494fb6c0cc13f25a06a4
vinod281 <57266038+vinod281@users.noreply.github.com>
2024-09-12 13:54:41 +0530

    improve home page

--- Client/auction_system/src/pages/HomePage/HomePage.jsx
+++ Client/auction_system/src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
 import AuctionCard from '../../components/Cards/AuctionCard';
+import SearchBar from '../../components/SearchBar/Searchbar';
+import './homePage.css'
 
 export default function HomePage() {
     return (
         <>
         
         <Header/>
+        <SearchBar/>
+
+        <div className="cardContainer">
+
         <AuctionCard/>
+        <AuctionCard/>
+        <AuctionCard/>
+        <AuctionCard/>
+        <AuctionCard/>
+        <AuctionCard/>
+        <AuctionCard/>
+        <AuctionCard/>
+        
+        
+
+        </div>
+        
 
             <div>HomePage</div>
         </>
