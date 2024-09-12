e4113e8d8208c4c890a711c58a067504c2095304
vinod281 <57266038+vinod281@users.noreply.github.com>
2024-09-12 14:31:47 +0530

    align fix

--- Client/auction_system/src/components/Header/Header.jsx
+++ Client/auction_system/src/components/Header/Header.jsx
@@ -8,33 +8,34 @@ export default function Header() {
     return (
         <>
             <div className='headerContainer'>
-                
-                        <Col>
-                            <div className='auctionLogo'>
-                                <img src="images/ai_logo.jpg" alt="" />
-                            </div>
-                        </Col>
 
-                        <Col>
-                            <div className='mainbanner'>
-                                <img src="images/66d9c10852c6a.img.png" alt="" className='mainBannerImg' />
-                            </div>
-                        </Col>
-
-                        <Col><div className='headerText'>
-                            <p className='textContact'>Questions? Problem? Help?<br/>
-                                Bidding support is available M-F from 9 AM-5PM EST.<br/>
-                                Contact us or call (800) 536-1401 x131.
-                            </p>
-                            <p className='noticeText'>
-                                For customer service after 5PM EST and Saturdays<br/>
-                                10AM-5PM call 800-536-1401 option 1 from the menu.
-                            </p>
+                <Row>
+                    <Col>
+                        <div className='auctionLogo'>
+                            <img src="images/ai_logo.jpg" alt="" />
                         </div>
-                        </Col>
-                    
+                    </Col>
+
+                    <Col>
+                        <div className='mainbanner'>
+                            <img src="images/66d9c10852c6a.img.png" alt="" className='mainBannerImg' />
+                        </div>
+                    </Col>
+
+                    <Col><div className='headerText'>
+                        <p className='textContact'>Questions? Problem? Help?
+                            Bidding support is available M-F from 9 AM-5PM EST.
+                            Contact us or call (800) 536-1401 x131.
+                        </p>
+                        <p className='noticeText'>
+                            For customer service after 5PM EST and Saturdays
+                            10AM-5PM call 800-536-1401 option 1 from the menu.
+                        </p>
+                    </div>
+                    </Col>
 
 
+                </Row>
 
 
 
--- Client/auction_system/src/pages/HomePage/HomePage.jsx
+++ Client/auction_system/src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
+import NavBar from '../../components/NavBar/Navbar2';
 import AuctionCard from '../../components/Cards/AuctionCard';
 import SearchBar from '../../components/SearchBar/Searchbar';
 import './homePage.css'
+
 
 export default function HomePage() {
     return (
         <>
         
         <Header/>
+        
+        <NavBar/>
         <SearchBar/>
 
         <div className="cardContainer">
