1171a9caaa3024c2c0439d5d5f05d0d92b320f32
Ronath1 <ronathwijayasiri123@gmail.com>
2024-10-10 13:41:46 +0530

    Develop modified searchbar

--- Client/auction_system/src/components/SearchBar/Searchbar.jsx
+++ Client/auction_system/src/components/SearchBar/Searchbar.jsx
@@ -1,105 +1,88 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './searchbar.css';
 
-import './Searchbar.css';
-
-
-function SearchBar() 
-  {
-
+function SearchBar() {
   return (
     <>
+<div className='searchBarContainer'>
+    <div className=" d-none d-sm-block">
     <div className="searchContainer">
-    <div className="searchopt1-container">
-
-      <form>
-
-        <div className="searchItem-Container">
-          <label >Search For Items:</label>
-          <input
-            id="searchTerm"
-            type="text"
-            placeholder="Search For Items"
+      
+          
+                <div>
+                  <Form className="horizontal-form">
+                    
+                       <Form.Group controlId="searchTerm">
+                       <Form.Label>Search For Items:</Form.Label>
+                       <Form.Control type="text" placeholder="Search For Items" />
+                       </Form.Group> 
+                
+                
+             
             
-             />
-        </div>
 
-
-
-
-        <div className="zipcode-Container">
-          <label htmlFor="ZipCode">Zipcode</label>
-
-          <input
-            className='spaced-zip'
-            id="ZipCode"
-            type="text"
-            placeholder="Zip Code"
+                       <Form.Group controlId="ZipCode">
+                       <Form.Label>Zip Code</Form.Label>
+                       <Form.Control type="text" placeholder="Zip Code"  />
+                       </Form.Group>
+                
+             
             
-             />
-        </div>
 
+                      <Form.Group controlId="Distance">
+                      <Form.Label>Distance</Form.Label>
+                      <Form.Select>
+                        <option value="5 Miles">5 Miles</option>
+                        <option value="10 Miles">10 Miles</option>
+                        <option value="25 Miles">25 Miles</option>
+                        <option value="50 Miles">50 Miles</option>
+                        <option value="100 Miles">100 Miles</option>
+                        <option value="250 Miles">250 Miles</option>
+                        <option value="500 Miles">500 Miles</option>
+                      </Form.Select>
+                      </Form.Group>
 
-        <div className="distanceContainer">
-          <label htmlFor="Distance">Distance</label>
+                      <Button variant="secondary" type="submit" className="btn1">Go</Button>
 
-          <select
-            id="Distance"
-            
-           >
+                      
+           
+                      </Form>
 
-            <option value="5 Miles">5 Miles</option>
-            <option value="10 Miles">10 Miles</option>
-            <option value="25 Miles">25 Miles</option>
-            <option value="50 Miles">50 Miles</option>
-            <option value="100 Miles">100 Miles</option>
-            <option value="250 Miles">250 Miles</option>
-            <option value="500 Miles">500 Miles</option>
+                      </div>
+                      
 
-          </select>
-        </div>
-        <div className="button1">
+            <div>
+                 <Form className="horizontal-form">
+                      
+                      <Form.Group controlId="VehicleType">
+                      <Form.Label>Vehicle Type</Form.Label>
+                      <Form.Select>
+                        <option value="">No Category Selected</option>
+                        <option value="Car">Car</option>
+                        <option value="Truck">Truck</option>
+                        <option value="Hatchback">Hatchback</option>
+                        <option value="Coupe">Coupe</option>
+                        <option value="Van">Van</option>
+                        <option value="SUV">SUV</option>
+                      </Form.Select>
+                      </Form.Group>
 
+  
+                      <Button variant="secondary" type="submit" className="btn1">Go</Button>
+                      <Button variant="secondary" type="button" className="btn2">Show All Items</Button>
+                 </Form>
+                 </div>
 
-          <button>Go</button>
-        </div>
-      </form>
+                 
+                  
+    
     </div>
-    <div className="searchopt2-container">
-
-        <form>
-
-          <div className="catagory-container">
-
-            <label htmlFor="Vehicle type">Vehicle type</label>
-
-            <select id="VehicleType">
-              
-              
-              <option value="">No Category Selected</option>
-              <option value="Car">Car</option>
-              <option value="Truck">Truck</option>
-              <option value="Hatchback">Hatchback</option>
-              <option value="coupe">coupe</option>
-              <option value="van">van</option>
-              <option value="suv">suv</option>
-
-            </select>
-          </div>
-
-          <div className="button2">
-            <button >Go</button>
-            </div>
-            <div className="button3">
-
-            <button >Show All Items</button>
-          </div>
-
-        </form>
-
-      </div>
-      </div>
-      </>
+    </div>
+    </div>
+    </>
   );
-};
-
+}
 
 export default SearchBar;
